6ea01fd651bd465bce2ced35161d2e75187dcd73
Ashleigh McMahon <ashleighmc@me.com>
2023-11-01 10:55:04 -0600

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = 'bob';
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+  }
+
+  function setBestCustomer() {
+    window.bestCustomer = 'not bob';
+  }
+
+  function overwriteBestCustomer(newBestCustomer) {
+    window.bestCustomer = newBestCustomer;
+  }
+
+  function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'new least favorite customer';
+  }
+
+const leastFavoriteCustomer = 'new least favorite customer';
+
+
+
